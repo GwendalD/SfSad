205cd39dbb99374358d8f2b03266a3eaf5701b89
gwendal <gwendaldugue@gmail.com>
2017-05-11 12:09:59 +0200

    Add Core Bundle to glue every bundle for the app

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new SF\PlatformBundle\SFPlatformBundle(),
+            new SF\CoreBundle\SFCoreBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
---
+++ src/SF/CoreBundle/SFCoreBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace SF\CoreBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class SFCoreBundle extends Bundle
+{
+}
